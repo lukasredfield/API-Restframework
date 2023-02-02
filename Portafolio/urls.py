5c8dd19c2b2611bb9611cd386dcd03b000bca594
lukasredfield <lukasredfield@gmail.com>
2023-02-02 18:46:34 -0300

    save05

--- Portafolio/urls.py
+++ Portafolio/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from App import views
 from django.contrib import admin
 from django.urls import path, include
 from reservas.router import router
